870be6543a9350f569d14471afb8b846e52f6d36
Eduardo Froes <eduardofroes95@gmail.com>
2023-03-19 16:53:23 -0300

    MDC

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+let calcularMdc = (a, b) => {
+    let resto;
+    while (b !== 0) {
+      resto = a % b;
+      a = b;
+      b = resto;
+    }
+    return a;
+  }
+  
+  const a = 20;
+  const b = 50;
+  const mdc = calcularMdc(a, b);
+  console.log(mdc)
